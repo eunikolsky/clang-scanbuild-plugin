0f925781d6a3adf40aed73b407324727c6ea1174
Eugene Nikolsky <eunikolsky@gmail.com>
2015-01-28 16:19:44 -0800

    Allow to use the plugin for any kind of project

--- src/main/java/jenkins/plugins/clangscanbuild/publisher/ClangScanBuildPublisherDescriptor.java
+++ src/main/java/jenkins/plugins/clangscanbuild/publisher/ClangScanBuildPublisherDescriptor.java
@@ -5,7 +5,6 @@ import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
 
 import hudson.model.AbstractProject;
-import hudson.model.FreeStyleProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Publisher;
 
@@ -39,10 +38,7 @@ public class ClangScanBuildPublisherDescriptor extends BuildStepDescriptor<Publi
 
 	@Override
 	public boolean isApplicable( @SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType ){
-		if( !FreeStyleProject.class.isAssignableFrom( jobType ) ){
-			System.err.println( "Clang scan-build ERROR: Expected FreeStyleProject but was: " + jobType + " at Publisher Descriptor" );
-		}
-		return FreeStyleProject.class.isAssignableFrom( jobType );
+		return true;
 	}
 
 }
